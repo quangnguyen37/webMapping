3d724fce2393c7dba9c6f460757230145a4641e6
quangnguyen37 <quanghng37@gmail.com>
2023-03-09 18:18:47 -0800

    Add map color based on population

--- map1.py
+++ map1.py
@@ -19,16 +19,25 @@ def color_elevation(elevation):
     else:
         return 'red'
     
-map = folium.Map(location=[37.774929,-122.419418], zoom_start=9, tiles = "Stamen Terrain")
+map = folium.Map(location=[37.774929,-122.419418], zoom_start=9, tiles = 'Stamen Toner')
 
 fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el in zip(lat, lon, elevation):
-    iframe = folium.IFrame(html=html % str(el), width=150, height=75)
-    fg.add_child(folium.Marker(location=[lt, ln], popup= folium.Popup(iframe),tooltip="Rachel is cute", icon=folium.Icon(color=color_elevation(el))))
+    iframe = folium.IFrame(html=html % str(el), width=200, height=100)
+    fg.add_child(folium.CircleMarker(location=[lt, ln], popup= folium.Popup(iframe),tooltip="Rachel is cute", radius= 6, fill =True, 
+                                     fill_opacity = 1 , fillColor=color_elevation(el), color = 'grey'))
 
 
-
+fg.add_child(folium.GeoJson(data = open('world.json', 'r', encoding= 'utf-8-sig').read(), 
+                            style_function= lambda x: {'fillColor':'yellow' if x['properties'] ['POP2005'] < 1000000
+                                                       else 'green' if 1000000 <= x['properties']['POP2005'] < 5000000
+                                                       else 'blue' if 5000000 <= x['properties']['POP2005'] < 10000000
+                                                       else 'purple' if 10000000 <= x['properties']['POP2005'] <50000000
+                                                       else 'black'if 50000000 <= x['properties']['POP2005'] <100000000
+                                                       else 'red', 
+                                                       'lineColor': '#FFFFFF', 
+                                                       'weight': '0.2'}))
 
 
 map.add_child(fg)
