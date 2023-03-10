50dab07c20292a545a5121869994113ba07346dc
quangnguyen37 <quanghng37@gmail.com>
2023-03-09 17:21:14 -0800

    Added all volcanoes makers

--- map1.py
+++ map1.py
@@ -1,9 +1,21 @@
 import folium
+import pandas
+
+vol_coord = pandas.read_csv("Volcanoes.txt")
+
+lat = list(vol_coord["LAT"])
+lon = list(vol_coord["LON"])
+elevation = list(vol_coord["ELEV"])
 
 map = folium.Map(location=[37.774929,-122.419418], zoom_start=9, tiles = "Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[37.297140, -121.833370], popup="This is a marker",tooltip="Rachel is cute", icon=folium.Icon(color="red")))
+
+for lt, ln, el in zip(lat, lon, elevation):
+    fg.add_child(folium.Marker(location=[lt, ln], popup= str(el) + " Meters",tooltip="Rachel is cute", icon=folium.Icon(color="red")))
+
+
+
 map.add_child(fg)
 
 map.save("Map1.html")
