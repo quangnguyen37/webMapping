f26ea38dcc5393837194261ec2497e3114040c65
quangnguyen37 <quanghng37@gmail.com>
2023-03-09 17:31:42 -0800

    Marker color changes according to elevation

--- map1.py
+++ map1.py
@@ -7,12 +7,27 @@ lat = list(vol_coord["LAT"])
 lon = list(vol_coord["LON"])
 elevation = list(vol_coord["ELEV"])
 
+html = """<h5>Volcano information:</h5>
+<h5>Height: %s meters</h5>
+"""
+
+def color_elevation(elevation):
+    if elevation < 1000:
+        return 'lightgreen'
+    if elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+    
 map = folium.Map(location=[37.774929,-122.419418], zoom_start=9, tiles = "Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el in zip(lat, lon, elevation):
-    fg.add_child(folium.Marker(location=[lt, ln], popup= str(el) + " Meters",tooltip="Rachel is cute", icon=folium.Icon(color="red")))
+    iframe = folium.IFrame(html=html % str(el), width=150, height=75)
+    fg.add_child(folium.Marker(location=[lt, ln], popup= folium.Popup(iframe),tooltip="Rachel is cute", icon=folium.Icon(color=color_elevation(el))))
+
+
 
 
 
