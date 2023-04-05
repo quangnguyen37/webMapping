d84fd102a182c0b5b14df64744120e914364d101
quangnguyen37 <quanghng37@gmail.com>
2023-04-04 18:24:18 -0700

    Added world volcano data and formatting

--- map1.py
+++ map1.py
@@ -1,14 +1,18 @@
 import folium
 import pandas
 
-vol_coord = pandas.read_csv("Volcanoes.txt")
+vol_coord = pandas.read_csv("volcano.csv")
 
 lat = list(vol_coord["LAT"])
-lon = list(vol_coord["LON"])
-elevation = list(vol_coord["ELEV"])
+lon = list(vol_coord["LONG"])
+volcanoName = list(vol_coord["VOLCANO"])
+lastErup = list(vol_coord["ERUPTED"])
+elevation = list(vol_coord["ELEV_M"])
 
 html = """<h5>Volcano information:</h5>
-<h5>Height: %s meters</h5>
+<body>Height: %s meters</body>
+<br>
+<body>Last Erupted: %s </body>
 """
 
 def color_elevation(elevation):
@@ -21,15 +25,16 @@ def color_elevation(elevation):
     
 map = folium.Map(location=[37.774929,-122.419418], zoom_start=9, tiles = 'Stamen Toner')
 
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 
-for lt, ln, el in zip(lat, lon, elevation):
-    iframe = folium.IFrame(html=html % str(el), width=200, height=100)
-    fg.add_child(folium.CircleMarker(location=[lt, ln], popup= folium.Popup(iframe),tooltip="Rachel is cute", radius= 6, fill =True, 
+for lt, ln, el, name, date in zip(lat, lon, elevation, volcanoName,lastErup):
+    iframe = folium.IFrame(html=html % (str(el), str(date)), width=200, height=100)
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], popup= folium.Popup(iframe),tooltip= name , radius= 6, fill =True, 
                                      fill_opacity = 1 , fillColor=color_elevation(el), color = 'grey'))
 
+fgp = folium.FeatureGroup(name="Population")
 
-fg.add_child(folium.GeoJson(data = open('world.json', 'r', encoding= 'utf-8-sig').read(), 
+fgp.add_child(folium.GeoJson(data = open('world.json', 'r', encoding= 'utf-8-sig').read(), 
                             style_function= lambda x: {'fillColor':'yellow' if x['properties'] ['POP2005'] < 1000000
                                                        else 'green' if 1000000 <= x['properties']['POP2005'] < 5000000
                                                        else 'blue' if 5000000 <= x['properties']['POP2005'] < 10000000
@@ -39,7 +44,10 @@ fg.add_child(folium.GeoJson(data = open('world.json', 'r', encoding= 'utf-8-sig'
                                                        'lineColor': '#FFFFFF', 
                                                        'weight': '0.2'}))
 
+map.add_child(fgv)
+map.add_child(fgp)
 
-map.add_child(fg)
+map.add_child(folium.LayerControl())
+
 
 map.save("Map1.html")
